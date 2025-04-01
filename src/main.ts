9b8b22dbeeefdf8e41ea372b350bf2ae1eca1e73
sajeebro <sajithmadusanka720@gmail.com>
2025-04-01 15:38:33 +0530

    initial

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        protoPath: join(__dirname, '../restaurant.proto'),
+        package: 'restaurant',
+        url: 'localhost:50057',
+      },
+    },
+  );
+  app.enableShutdownHooks();
+  await app.listen();
+  console.log('Restaurant service is running on: http://localhost:50057');
+}
+void bootstrap();
