27eba6ee5f8f1cdc434bcaece43e69a4ca0155ba
sajeebro <sajithmadusanka720@gmail.com>
2025-04-03 10:24:06 +0530

    added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RestaurantModule } from 'restaurant/restaurant.module';
 
 @Module({
-  imports: [RestaurantModule],
+  imports: [
+    RestaurantModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/default-db',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
